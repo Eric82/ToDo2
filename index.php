5935340a38e498d5342eeea9cf988eb31eafec6c
Eric Martinez <martinez_eric82@yahoo.com>
2015-04-20 18:01:12 -0700

    completed video 10

--- index.php
+++ index.php
@@ -24,11 +24,10 @@
 		$('add-new-task').submit(function() {
 			var new_task = $('.add-new-task input[name=new-task]').val();
 
-			if (new_task) {
+			if (new_task != '') {
 				$.post('includes/add-task.php', { task: new_task}, function(data){
-					$(('add-new-task input[name=new-task]').val();
+					$('add-new-task input[name=new-task]').val();
 						$(data).appendTo('task-list ul').hide().fadeIn();
-
 				});
 			}		
 			return false;	
