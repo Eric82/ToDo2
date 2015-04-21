7bdf6d1aa41595f99856fd6ce3889fb80cd5cacb
Eric Martinez <martinez_eric82@yahoo.com>
2015-04-20 17:18:48 -0700

    completed video 2

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Eric's Todo2</title>
+	<link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body>
+	<div class="wrap">
+	
+	</div>
+</body>
+</html>
